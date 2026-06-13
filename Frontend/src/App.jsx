d3399da5030dd8fb49543721148f76b62a040f13
nahid-105-ontor <ontornahidhasan65@gmail.com>
2026-06-13 17:02:23 +0600

    edited

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,41 @@
+import {useState } from "react";
+
 function App(){
-    return(
-        <div>
-            <h1 className="text-danger">Hello World</h1>
-        </div>
+    const[data,setData]= useState([]);
+
+  function handleButton() {
+    fetch("/api/jokes", {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+    return (
+      <div>
+        <button className="btn btn-info ms-2 mt-2" onClick={handleButton}>
+          Get Jokes
+        </button>
+        <p>Jokes : {data.length}</p>
+        {data === null ? (
+          <p className="text-danger">No Jokes Found</p>
+        ) : data.length > 0 ? (
+          data.map((joke) => (
+            <div className="m-3" key={joke.id}>
+                <h5>{joke.id}</h5>
+              <h3>{joke.title}</h3>
+              <p>{joke.content}</p>
+            </div>
+          ))
+        ) : (
+          <p>No jokes available</p>
+        )}
+      </div>
     );
 }
 export default App;
